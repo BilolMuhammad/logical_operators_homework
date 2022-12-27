e65d4d6019f98159a9df73e6d05b263d3e48d2b2
PRESTIGE <mamatqulov@gmail.com>
2022-12-27 10:12:57 +0500

    17

--- logic17.py
+++ logic17.py
@@ -6,4 +6,16 @@ def main(a):
     Returns:
         bool: answer
     """
-    return
+    b1 = a % 10000
+    b2 = b1 % 1000
+    b3 = b2 % 100
+
+    a1 = a // 10000
+    a2 = b1 // 1000
+    a3 = b2 // 100
+    a4 = b3 // 10
+    a5 = b3 % 10
+    return a >= 10000 and a <= 99999 and a1 < a2 < a3 < a4 < a5 and a5 > a4 > a3 > a2 > a1
+
+
+print(main(56789))
