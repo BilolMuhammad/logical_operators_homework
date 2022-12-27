313920baa02c9d0156e4e9de401ce276acfa8f0e
PRESTIGE <mamatqulov@gmail.com>
2022-12-27 10:12:14 +0500

    18

--- logic18.py
+++ logic18.py
@@ -5,4 +5,15 @@ def main(a):
     Returns:
         bool: answer
     """
-    return
+    a1 = a // 10000
+    b1 = a % 10000
+    a2 = b1 // 1000
+    b2 = b1 % 1000
+    a3 = b2 // 100
+    b3 = b2 % 100
+    a4 = b3 // 10
+    a5 = b3 % 10
+    return a1 > a2 > a3 > a4 > a5 and a5 < a4 < a3 < a2 < a1
+
+
+print(main(54321))
