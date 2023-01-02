ca3d5acdddd26bf783e94e72c14e45dd5ace291f
PRESTIGE <mamatqulov@gmail.com>
2023-01-02 17:19:56 +0500

    18

--- logic18.py
+++ logic18.py
@@ -1,19 +1,22 @@
 def main(a):
-    """Given a five-digit integer a, check the following statement "All digits of the number are in descending order".
+    """Given a five-digit integer a, check the following statement "All digits of the number 
+    are in descending order".
+
     Args:
         a(int): parameter a
     Returns:
         bool: answer
     """
-    a1 = a // 10000
-    b1 = a % 10000
-    a2 = b1 // 1000
-    b2 = b1 % 1000
-    a3 = b2 // 100
-    b3 = b2 % 100
-    a4 = b3 // 10
-    a5 = b3 % 10
-    return a1 > a2 > a3 > a4 > a5 and a5 < a4 < a3 < a2 < a1
+    x1 = a % 10
+    a //= 10
+    x2 = a % 10
+    a //= 10
+    x3 = a % 10
+    a //= 10
+    x4 = a % 10
+    a //= 10
+    x5 = a % 10
+    return x1 < x2 and x2 < x3 and x3 < x4 and x4 < x5
 
 
 print(main(54321))
