dd69174dfdb10aede769a42c0264a5b78ccdc7cc
PRESTIGE <mamatqulov@gmail.com>
2023-01-08 11:58:54 +0500

    17

--- logic17.py
+++ logic17.py
@@ -17,7 +17,10 @@ def main(a):
     x4 = a % 10
     a //= 10
     x5 = a % 10
-    return x1 > x2 and x2 > x3 and x3 > x4 and x4 > x5
+    if x1 > x2 and x2 > x3 and x3 > x4 and x4 > x5:
+        return True
+    else:
+        return False
 
 
-print(main(56789))
+print(main(96789))
